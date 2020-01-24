2f2c211bb597b04eb089d8346032770ef5152217
Steven Sandoval <ssandova@gmail.com>
2020-01-24 09:28:01 -0700

    Added demo for CT timeshift property

---
+++ Chapter1/code/DEMO_timeShiftProperty.jl
@@ -0,0 +1,30 @@
+# This file demonstates the time shift property for CT signals
+
+using Plots, Interact
+
+Λ(t::Real) = ifelse(t < -1, zero(t), ifelse(t < 0, t+1, ifelse(t < 1, -t+1, zero(t)) ) ) #unit triangle function
+
+t = -5:0.01:10
+@manipulate for t₀ in -3:0.2:5
+
+    p1 = plot(t, Λ.(t),
+            ylims = (-1,2),
+            legend=:false,
+            xlab = "t",
+            ylab = "f(t)",
+            )
+
+    xlims = Vector(-5:8)
+    xticks = ( cat(xlims,t₀,dims=1), cat(string.(xlims),"t_0",dims=1) )
+    p2 = plot(t,
+        Λ.(t.-t₀),
+        framestyle = :origin,
+        ylims = (-1,2),
+        legend=:false,
+        xlab = "t",
+        ylab = "f(t-t_0)",
+        xticks=xticks,
+        )
+    plot(p1, p2, layout = (2,1) )
+
+end
