b7912dea08f54faff589cc02b5325207cf32253d
Steven Sandoval <ssandova@gmail.com>
2020-01-24 10:09:51 -0700

    added/updated time-shift demos

--- Chapter1/code/DEMO_timeShiftPropertyCT.jl
+++ Chapter1/code/DEMO_timeShiftPropertyCT.jl
@@ -1,13 +1,12 @@
-# This file demonstates the time shift property for CT signals
-
+# This file demonstates the time-shift property for CT signals
 using Plots, Interact
 
-Λ(t::Real) = ifelse(t < -1, zero(t), ifelse(t < 0, t+1, ifelse(t < 1, -t+1, zero(t)) ) ) #unit triangle function
+f(t::Real) = ifelse(t < -1, zero(t), ifelse(t < 0, t+1, ifelse(t < 1, -t+1, zero(t)) ) ) #unit triangle function
 
 t = -5:0.01:10
 @manipulate for t₀ in -3:0.2:5
 
-    p1 = plot(t, Λ.(t),
+    p1 = plot(t, f.(t),
             framestyle = :origin,
             ylims = (-1,2),
             legend=:false,
@@ -18,7 +17,7 @@ t = -5:0.01:10
     xlims = Vector(-5:8)
     xticks = ( cat(xlims,t₀,dims=1), cat(string.(xlims),"t_0",dims=1) )
     p2 = plot(t,
-        Λ.(t.-t₀),
+        f.(t.-t₀),
         framestyle = :origin,
         ylims = (-1,2),
         legend=:false,
---
+++ Chapter1/code/DEMO_timeShiftPropertyDT.jl
@@ -0,0 +1,40 @@
+# This file demonstates the time-shift property for DT signals
+using Plots, Interact
+
+u(n::Integer) = ifelse(n < 0, zero(n), one(n)) #unit step function
+f(n::Integer) = u(n)-u(n-3) #unit step function
+
+n = -5:10
+@manipulate for n₀ in -3:5
+
+    p1 = plot(n,
+            f.(n),
+            framestyle = :origin,
+            ylims = (-1,2),
+            legend=:false,
+            xlab = "n",
+            ylab = "u(n)",
+            line = :stem,
+            marker = :circle,
+            color = :blue,
+            markercolor = :blue,
+            )
+
+    xlims = Vector(-5:8)
+    xticks = ( cat(xlims,n₀,dims=1), cat(string.(xlims),"n_0",dims=1) )
+    p2 = plot(n,
+        f.(n.-n₀),
+        framestyle = :origin,
+        ylims = (-1,2),
+        legend=:false,
+        xlab = "n",
+        ylab = "u(n-n_0)",
+        xticks=xticks,
+        line = :stem,
+        marker = :circle,
+        color = :blue,
+        markercolor = :blue,
+        )
+    plot(p1, p2, layout = (2,1) )
+
+end
