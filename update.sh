3e5995375fdc8342816a9f37cd0b835ba4e3a43f
Andrew McGinnis <andrew@mcginnis.space>
2020-02-21 12:10:11 -0700

    Fixed repository name typo

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # update.sh
 # This script tries to pull the latest changes from upstream and copy
 # them over to this repository without overwriting existing files.
-git pull https://github.com/spsandova/S-SI
+git pull https://github.com/ssandova/S-SI
 
 # Allow commiting selectively
 git commit -p
