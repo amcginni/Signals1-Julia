890803aa2b5de900ddea1bbc7640af3c3c9e2e13
Andrew McGinnis <andrew@mcginnis.space>
2020-02-21 12:08:52 -0700

    Update repository update script

--- update.sh
+++ update.sh
@@ -2,9 +2,12 @@
 # update.sh
 # This script tries to pull the latest changes from upstream and copy
 # them over to this repository without overwriting existing files.
-git pull ../S-SI
-cp -ur ../S-SI/* .
+git pull https://github.com/spsandova/S-SI
 
-# Use interactive mode for git-add, so we can selectively add files to be
-# commited.
-git add -i
+# Allow commiting selectively
+git commit -p
+
+#Revert uncommited changes to directory tree
+git reset HEAD
+
+echo("If the commit was succesful, now do a git push");
